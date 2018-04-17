1ecdf958f5568bc2ef47d8283a1645b69e09284e
vagrant <vagrant@homestead>
2018-04-17 06:41:18 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,10 +1,18 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>@yield('title','sample') app</title>
+    <title>@yield('title', 'Sample App') - Laravel </title>
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-@yield('content')
+@include('layouts._header')
+
+<div class="container">
+    <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+    </div>
+</div>
+
 </body>
 </html>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+
+@extends('layouts.default')
+@section('title','signup')
+@section('content')
+    <h1>注册</h1>
+@stop
